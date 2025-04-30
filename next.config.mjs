1d28ba3f957e856e641d597db3643f7ac83c1d37
Masamitsu Bando <bando@ktc.ac.jp>
2025-04-30 18:31:22 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: "/jact2025",
+  basePath: "/jact2025",
+  trailingSlash: true,
 };
 
 export default nextConfig;
